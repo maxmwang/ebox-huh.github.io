117097412168b312081d67f77c9e7cdbabab7064
ebox-huh <differentmax9@gmail.com>
2022-03-23 03:05:31 -0700

    Updates

---
+++ precache-manifest.2724429adaf6b642bc2d1041f1d18b13.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "58af12ab7135c48acad7",
+    "url": "/anman-list/static/css/main.f6ca5bea.chunk.css"
+  },
+  {
+    "revision": "58af12ab7135c48acad7",
+    "url": "/anman-list/static/js/main.47158ee8.chunk.js"
+  },
+  {
+    "revision": "0ef62ee474052f2f088e",
+    "url": "/anman-list/static/js/runtime~main.038fa1a2.js"
+  },
+  {
+    "revision": "f85eb02d91ea5f630906",
+    "url": "/anman-list/static/js/2.39d4758c.chunk.js"
+  },
+  {
+    "revision": "ba9dc2832ebb8404768d3f23be3814eb",
+    "url": "/anman-list/index.html"
+  }
+];
